4d73c5cd18b62c920637a50bbc7648de6579df06
Maragon <51925459+MaragonMH@users.noreply.github.com>
2025-01-27 15:47:24 +0100

    Update regions

--- worlds/xenobladex/regions/key.py
+++ worlds/xenobladex/regions/key.py
@@ -4,6 +4,7 @@ from ..Regions import Requirement as Req, Rule
 # flake8: noqa
 doll_regions: List[Rule] = [
 Rule("Menu"),
+Rule("Blade License", {Req("KEY: Blade License")}),
 Rule("Skell License", {Req("KEY: Skell License")}),
 Rule("Flight Module", {Req("KEY: Flight Module")}),
 ]
