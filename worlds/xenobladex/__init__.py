89b558e2bfb31c51443536e5b905d3cfb8b9c234
Maragon <51925459+MaragonMH@users.noreply.github.com>
2026-09-03 19:36:36 +0200

    Prepare for next version

--- worlds/xenobladex/__init__.py
+++ worlds/xenobladex/__init__.py
@@ -39,7 +39,7 @@ class XenobladeXWorld(CachedRuleBuilderWorld):
     topology_present = True
     web = XenobladeXWeb()
 
-    data_version = 16
+    data_version = 17
     base_id: int = 4100000
     ut_can_gen_without_yaml = True
 
